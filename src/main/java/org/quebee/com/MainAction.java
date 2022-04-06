67a0d9d2992b60eef5eddf9dc8749e0b0e3fd5c6
Ilia_Stavrov <petromarz@yandex.ru>
2022-04-06 20:26:39 +0300

    test panel

--- src/main/java/org/quebee/com/MainAction.java
+++ src/main/java/org/quebee/com/MainAction.java
@@ -2,14 +2,22 @@ package org.quebee.com;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.ui.MessageDialogBuilder;
+import com.intellij.openapi.ui.DialogPanel;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class MainAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        MessageDialogBuilder.yesNo("Test", "Test").ask(e.getProject());
+        DialogPanel p = DemoTipsKt.demoTips(e.getProject());
+        JFrame f = new JFrame();
+        f.setLayout(new BorderLayout());
+        f.add(p, BorderLayout.CENTER);
+        f.pack();
+        f.setVisible(true);
     }
 
     @Override
