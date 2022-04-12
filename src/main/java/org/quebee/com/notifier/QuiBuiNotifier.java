8d148922cddc0d7d9b9377a3806058cbcb506145
Ilia_Stavrov <petromarz@yandex.ru>
2022-04-12 14:34:15 +0300

    selected field

--- src/main/java/org/quebee/com/notifier/QuiBuiNotifier.java
+++ src/main/java/org/quebee/com/notifier/QuiBuiNotifier.java
@@ -1,11 +1,12 @@
 package org.quebee.com.notifier;
 
 import com.intellij.util.messages.Topic;
+import org.quebee.com.model.TableElement;
 
 public interface QuiBuiNotifier {
 
     Topic<QuiBuiNotifier> QUI_BUI_TOPIC = Topic.create("qui bui topic", QuiBuiNotifier.class);
 
-    void onAction(Object context);
+    void onAction(TableElement context);
 
 }
--- src/main/java/org/quebee/com/panel/FromTables.java
+++ src/main/java/org/quebee/com/panel/FromTables.java
@@ -8,19 +8,17 @@ import com.intellij.ui.ToolbarDecorator;
 import com.intellij.ui.treeStructure.treetable.ListTreeTableModel;
 import com.intellij.ui.treeStructure.treetable.TreeColumnInfo;
 import com.intellij.ui.treeStructure.treetable.TreeTable;
-import com.intellij.ui.treeStructure.treetable.TreeTableModel;
 import com.intellij.util.IconUtil;
 import com.intellij.util.messages.MessageBus;
 import com.intellij.util.ui.ColumnInfo;
 import org.jdesktop.swingx.treetable.DefaultMutableTreeTableNode;
+import org.jdesktop.swingx.treetable.MutableTreeTableNode;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.quebee.com.database.DBTables;
 import org.quebee.com.model.TableElement;
 import org.quebee.com.notifier.QuiBuiNotifier;
-import org.quebee.com.database.DBTables;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
@@ -28,12 +26,14 @@ import java.util.Map;
 import java.util.Random;
 
 import static org.quebee.com.notifier.QuiBuiNotifier.QUI_BUI_TOPIC;
-import static org.quebee.com.panel.OrderPanel.getTitleColumnInfo;
 import static org.quebee.com.notifier.ReloadDbTablesNotifier.RELOAD_TABLES_TOPIC;
 
 public class FromTables {
     public static final String HEADER = "Tables and Fields";
     public JComponent element;
+
+    private DefaultMutableTreeTableNode databaseRoot;
+    private ListTreeTableModel databaseModel;
 
     public FromTables() {
         JBSplitter splitter = new JBSplitter();
@@ -41,8 +41,8 @@ public class FromTables {
 
         splitter.setFirstComponent(databaseTables());
         JBSplitter splitter2 = new JBSplitter();
-        splitter2.setFirstComponent(treeTable("Tables"));
-        splitter2.setSecondComponent(treeTable("Fields"));
+        splitter2.setFirstComponent(selectedTables());
+        splitter2.setSecondComponent(selectedFields());
         splitter.setSecondComponent(splitter2);
         this.element = splitter;
         init(ApplicationManager.getApplication().getMessageBus());
@@ -50,9 +50,43 @@ public class FromTables {
 
     public void init(MessageBus bus) {
         bus.connect().subscribe(RELOAD_TABLES_TOPIC, this::setDatabaseTables);
+        bus.connect().subscribe(QUI_BUI_TOPIC, this::addSelectedTable);
     }
 
-    public JComponent treeTable(String tableName) {
+    private void addSelectedTable(TableElement o) {
+        selectedTablesRoot.add(new DefaultMutableTreeTableNode(o));
+        selectedTablesModel.reload();
+    }
+
+    DefaultMutableTreeTableNode selectedTablesRoot;
+    ListTreeTableModel selectedTablesModel;
+
+    public JComponent selectedTables() {
+        selectedTablesRoot = new DefaultMutableTreeTableNode(new TableElement("empty"));
+        selectedTablesModel = new ListTreeTableModel(selectedTablesRoot, new ColumnInfo[]{
+                new TreeColumnInfo("Tables")
+        });
+        var treeTable = new TreeTable(selectedTablesModel);
+        treeTable.setRootVisible(false);
+        treeTable.setTreeCellRenderer(new TableElement.Renderer());
+        //  JBScrollPane jbScrollPane = new JBScrollPane(treeTable);
+
+        var decorator = ToolbarDecorator.createDecorator(treeTable);
+
+        decorator.setAddAction(button -> {
+//            root.insert(new DefaultMutableTreeTableNode("test" + new Random(1000).nextInt()), i.get());
+//            model.nodesWereInserted(root, new int[]{i.get()});
+            selectedTablesRoot.add(getTest("test" + new Random(1000).nextInt()));
+            selectedTablesModel.reload();
+        });
+        decorator.setRemoveAction(button -> {
+            System.out.println(button);
+            // myTableModel.addRow();
+        });
+        return decorator.createPanel();
+    }
+
+    public JComponent selectedFields() {
 
         DefaultMutableTreeTableNode root = getTest("test");
 
@@ -67,7 +101,7 @@ public class FromTables {
         root.add(getTest("test 4"));
 
         ListTreeTableModel model = new ListTreeTableModel(root, new ColumnInfo[]{
-                getTitleColumnInfo(tableName)
+                new TreeColumnInfo("Fields")
         });
         TreeTable treeTable = new TreeTable(model);
         treeTable.setTreeCellRenderer(new TableElement.Renderer());
@@ -93,26 +127,23 @@ public class FromTables {
         return new DefaultMutableTreeTableNode(new TableElement(test));
     }
 
-    private DefaultMutableTreeTableNode databaseRoot;
-    private ListTreeTableModel databaseModel;
-
     public JComponent databaseTables() {
-
         databaseRoot = new DefaultMutableTreeTableNode(new TableElement("tables"));
-
         databaseModel = new ListTreeTableModel(databaseRoot, new ColumnInfo[]{
                 new TreeColumnInfo("Database")
         });
-        TreeTable treeTable = new TreeTable(databaseModel);
+        final TreeTable treeTable = new TreeTable(databaseModel);
+
         treeTable.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent mouseEvent) {
                 TreeTable table = (TreeTable) mouseEvent.getSource();
-                Point point = mouseEvent.getPoint();
                 if (mouseEvent.getClickCount() == 2 && table.getSelectedRow() != -1) {
                     MessageBus messageBus = ApplicationManager.getApplication().getMessageBus();
                     QuiBuiNotifier publisher = messageBus.syncPublisher(QUI_BUI_TOPIC);
-                    publisher.onAction(table.getSelectedRow());
+                    Object userObject = ((MutableTreeTableNode) treeTable.getValueAt(table.getSelectedRow(), 0)).getUserObject();
+                    TableElement value1 = (TableElement) userObject;
+                   publisher.onAction(value1);
                 }
             }
         });
