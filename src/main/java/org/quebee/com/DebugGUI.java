5bcb76a35beabbc6230360786a20693e576e59b2
Ilia_Stavrov <petromarz@yandex.ru>
2022-04-14 19:30:05 +0300

    parser

--- src/main/java/org/quebee/com/DebugGUI.java
+++ src/main/java/org/quebee/com/DebugGUI.java
@@ -2,11 +2,20 @@ package org.quebee.com;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.Statement;
 import org.jetbrains.annotations.NotNull;
 
 public class DebugGUI implements StartupActivity {
     @Override
     public void runActivity(@NotNull Project project) {
+        try {
+            Statement parse = CCJSqlParserUtil.parse("select id from table where id = 1");
+            System.out.println(parse);
+        } catch (JSQLParserException exception) {
+
+        }
         MainQuiBuiForm form = new MainQuiBuiForm(project);
         form.show();
     }
