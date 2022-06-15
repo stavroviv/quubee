a8f11978015694eab661b13c6d76bc3971afdbf0
Ilia_Stavrov <petromarz@yandex.ru>
2022-06-15 11:18:59 +0200

    conditions panel

--- src/main/java/org/quebee/com/panel/ConditionsPanel.java
+++ src/main/java/org/quebee/com/panel/ConditionsPanel.java
@@ -2,7 +2,6 @@ package org.quebee.com.panel;
 
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.ui.ComboBox;
 import com.intellij.openapi.ui.TreeComboBox;
 import com.intellij.ui.AnActionButton;
@@ -13,7 +12,6 @@ import com.intellij.ui.table.TableView;
 import com.intellij.ui.treeStructure.treetable.ListTreeTableModel;
 import com.intellij.ui.treeStructure.treetable.TreeColumnInfo;
 import com.intellij.ui.treeStructure.treetable.TreeTable;
-import com.intellij.util.messages.MessageBus;
 import com.intellij.util.ui.AbstractTableCellEditor;
 import com.intellij.util.ui.ColumnInfo;
 import com.intellij.util.ui.ListTableModel;
@@ -26,6 +24,7 @@ import org.quebee.com.model.QBTreeNode;
 import org.quebee.com.model.TableElement;
 import org.quebee.com.model.TreeComboTableElement;
 import org.quebee.com.notifier.SelectedTableAfterAddNotifier;
+import org.quebee.com.notifier.SelectedTableRemoveNotifier;
 
 import javax.swing.*;
 import javax.swing.border.LineBorder;
@@ -43,14 +42,6 @@ public class ConditionsPanel extends AbstractQueryPanel {
         component.setProportion(0.3f);
         component.setFirstComponent(getFieldsTree());
         component.setSecondComponent(getConditionsTable());
-        init(ApplicationManager.getApplication().getMessageBus());
-    }
-
-    public void init(MessageBus bus) {
-//        bus.connect().subscribe(QUI_BUI_TOPIC, context -> {
-//            System.out.println(context);
-//            System.out.println(context);
-//        });
     }
 
     private JComponent getConditionsTable() {
@@ -58,20 +49,20 @@ public class ConditionsPanel extends AbstractQueryPanel {
 
         var conditionInfo = new ColumnInfo<ConditionElement, ConditionElement>("Condition") {
             @Override
-            public @NotNull ConditionElement valueOf(ConditionElement templateVariable) {
-                return templateVariable;
+            public @NotNull ConditionElement valueOf(ConditionElement element) {
+                return element;
             }
 
             @Override
-            public void setValue(ConditionElement conditionElement, ConditionElement value) {
-                conditionElement.setCondition(value.getCondition());
-                conditionElement.setConditionComparison(value.getConditionComparison());
-                conditionElement.setConditionRight(value.getConditionRight());
-                conditionElement.setConditionLeft(value.getConditionLeft());
+            public void setValue(ConditionElement element, ConditionElement value) {
+                element.setCondition(value.getCondition());
+                element.setConditionComparison(value.getConditionComparison());
+                element.setConditionRight(value.getConditionRight());
+                element.setConditionLeft(value.getConditionLeft());
             }
 
             @Override
-            public boolean isCellEditable(ConditionElement templateVariable) {
+            public boolean isCellEditable(ConditionElement element) {
                 return true;
             }
 
@@ -94,70 +85,7 @@ public class ConditionsPanel extends AbstractQueryPanel {
 
             @Override
             public @NotNull TableCellEditor getEditor(final ConditionElement variable) {
-                return new AbstractTableCellEditor() {
-                    private final TreeComboBox conditionLeftCombo = getTreeComboBox();
-
-                    @NotNull
-                    private TreeComboTableElement getTest(String test) {
-                        return new TreeComboTableElement(test, DatabaseIcons.Table);
-                    }
-
-                    @NotNull
-                    private TreeComboBox getTreeComboBox() {
-                        TreeComboTableElement root = getTest("test");
-
-                        final TreeComboTableElement child = getTest("test 1");
-                        for (int i = 0; i < 100; i++) {
-                            child.add(getTest("test 1" + i));
-                        }
-                        child.add(getTest("test 12"));
-                        root.add(child);
-                        root.add(getTest("test 2"));
-                        root.add(getTest("test 3"));
-                        root.add(getTest("test 4"));
-
-                        ListTreeTableModel model = new ListTreeTableModel(
-                                root,
-                                new ColumnInfo[]{
-                                        new TreeColumnInfo("Tables")
-                                });
-
-                        return new TreeComboBox(model, false);
-                    }
-
-                    private final JTextField conditionRight = new JTextField();
-                    private final ComboBox<String> comparisonCombo =
-                            new ComboBox<>(new String[]{"=", "!=", ">", "<", ">=", "<=", "like"});
-                    private final JTextField conditionCustom = new JTextField();
-
-                    @Override
-                    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
-                        if (variable.isCustom()) {
-                            conditionCustom.setText(variable.getCondition());
-                            return conditionCustom;
-                        }
-                        Box hBox = Box.createHorizontalBox();
-                        conditionLeftCombo.setPreferredSize(new Dimension(200, 15));
-                        hBox.add(conditionLeftCombo);
-                        comparisonCombo.setItem(variable.getConditionComparison());
-                        hBox.add(comparisonCombo);
-                        conditionRight.setText(variable.getConditionRight());
-                        hBox.add(conditionRight);
-                        return hBox;
-                    }
-
-                    @Override
-                    public Object getCellEditorValue() {
-                        ConditionElement elem = new ConditionElement();
-                        if (conditionLeftCombo.getSelectedItem() != null) {
-                            elem.setConditionLeft(conditionLeftCombo.getSelectedItem().toString());
-                        }
-                        elem.setConditionRight(conditionRight.getText());
-                        elem.setConditionComparison(comparisonCombo.getItem());
-                        elem.setCondition(conditionCustom.getText());
-                        return elem;
-                    }
-                };
+                return new CustomConditionEditor(variable);
             }
         };
 
@@ -201,9 +129,81 @@ public class ConditionsPanel extends AbstractQueryPanel {
         return decorator.createPanel();
     }
 
+    private class CustomConditionEditor extends AbstractTableCellEditor {
+
+        private final TreeComboBox conditionLeftCombo = getTreeComboBox();
+        private final ConditionElement variable;
+
+        public CustomConditionEditor(ConditionElement variable) {
+            this.variable = variable;
+        }
+
+        @NotNull
+        private TreeComboTableElement getTest(String test) {
+            return new TreeComboTableElement(test, DatabaseIcons.Table);
+        }
+
+        @NotNull
+        private TreeComboBox getTreeComboBox() {
+            TreeComboTableElement root = getTest("test");
+
+            final TreeComboTableElement child = getTest("test 1");
+            for (int i = 0; i < 100; i++) {
+                child.add(getTest("test 1" + i));
+            }
+            child.add(getTest("test 12"));
+            root.add(child);
+            root.add(getTest("test 2"));
+            root.add(getTest("test 3"));
+            root.add(getTest("test 4"));
+
+            ListTreeTableModel model = new ListTreeTableModel(
+                    root,
+                    new ColumnInfo[]{
+                            new TreeColumnInfo("Tables")
+                    });
+
+            return new TreeComboBox(model, false);
+        }
+
+        private final JTextField conditionRight = new JTextField();
+        private final ComboBox<String> comparisonCombo =
+                new ComboBox<>(new String[]{"=", "!=", ">", "<", ">=", "<=", "like"});
+        private final JTextField conditionCustom = new JTextField();
+
+        @Override
+        public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+            if (variable.isCustom()) {
+                conditionCustom.setText(variable.getCondition());
+                return conditionCustom;
+            }
+            Box hBox = Box.createHorizontalBox();
+            conditionLeftCombo.setPreferredSize(new Dimension(200, 15));
+            hBox.add(conditionLeftCombo);
+            comparisonCombo.setItem(variable.getConditionComparison());
+            hBox.add(comparisonCombo);
+            conditionRight.setText(variable.getConditionRight());
+            hBox.add(conditionRight);
+            return hBox;
+        }
+
+        @Override
+        public Object getCellEditorValue() {
+            ConditionElement elem = new ConditionElement();
+            if (conditionLeftCombo.getSelectedItem() != null) {
+                elem.setConditionLeft(conditionLeftCombo.getSelectedItem().toString());
+            }
+            elem.setConditionRight(conditionRight.getText());
+            elem.setConditionComparison(comparisonCombo.getItem());
+            elem.setCondition(conditionCustom.getText());
+            return elem;
+        }
+    };
+
     @Override
     public void initListeners() {
         subscribe(SelectedTableAfterAddNotifier.class, this::addSelectedTable);
+        subscribe(SelectedTableRemoveNotifier.class, this::removeSelectedTable);
     }
 
     private void addSelectedTable(QBTreeNode node) {
@@ -211,6 +211,22 @@ public class ConditionsPanel extends AbstractQueryPanel {
         var newTableNode = new QBTreeNode(newUserObject);
         node.nodeToList().forEach(x -> newTableNode.add(new QBTreeNode(x.getUserObject())));
         allFieldsRoot.add(newTableNode);
-        allFieldsModel.nodesWereInserted(allFieldsRoot, new int[]{allFieldsRoot.getChildCount() - 1});
+        if (allFieldsRoot.getChildCount() == 1) {
+            allFieldsModel.reload();
+        } else {
+            allFieldsModel.nodesWereInserted(allFieldsRoot, new int[]{allFieldsRoot.getChildCount() - 1});
+        }
+    }
+
+    private void removeSelectedTable(QBTreeNode node) {
+        var userObject = node.getUserObject();
+        var removeTableName = userObject.getDescription();
+        allFieldsRoot.nodeToList().stream()
+                .filter(x -> x.getUserObject().getDescription().equals(removeTableName))
+                .findFirst().ifPresent(x -> {
+                    var index = allFieldsRoot.getIndex(x);
+                    allFieldsRoot.remove(x);
+                    allFieldsModel.nodesWereRemoved(allFieldsRoot, new int[]{index}, new Object[]{x});
+                });
     }
 }
